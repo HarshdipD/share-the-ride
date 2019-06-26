3523d855d18586ba3ee88eeda78b0b33c7803381
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2019-06-26 12:17:12 -0400

    Added StaticPages controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    # adds the page name to the title followed by default 'SharetheRide'
+    def full_title(page_title)
+        base_title = 'ShareTheRide'
+        if page_title.empty?
+            base_title
+        else
+            page_title + ' | ' + base_title
+        end
+    end
+
+
 end
