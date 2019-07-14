82995f96e9e0ff91ab2b2c545bf820f91739b962
HarshdipD <harshdipsdeogan@gmail.com>
2019-07-13 22:00:35 -0400

    Added remember me functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < ApplicationController
   end
   
   def create 
-    #find the user in our User Db using find_by method
-    user = User.find_by(email: params[:session][:email].downcase)
+    #find the user in our User database using find_by method
+    @user = User.find_by(email: params[:session][:email].downcase)
     # authenticate the user password. 
-    if user && user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       # if everythig is corect then redirect it to user's show page
-      log_in user        
-      redirect_to user      # Here instead of redirecting to user We need to redirect it to a page where there is information of all the posts. This page is yet to be made
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)     
+      redirect_to @user      # Here instead of redirecting to user We need to redirect it to a page where there is information of all the posts. This page is yet to be made
     else 
       #show an error 
       flash.now[:danger] = 'Invalid email or password'
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
