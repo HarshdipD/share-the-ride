e4d33f66475ea4952192d119da6c8c908c902b12
HarshdipD <harshdipsdeogan@gmail.com>
2019-07-16 11:18:45 -0400

    add post at profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
+    @micropost  = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def create
